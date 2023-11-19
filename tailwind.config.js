4878dd8999052fc9dc8f9dd03e6a45d3858daf52
ynyahya <yoginuryahya.id@gmail.com>
2023-11-19 22:41:05 +0700

    project selesai

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      borderWidth: {
+        1.5: "1.5px",
+      },
+    },
+  },
+  plugins: [],
+};
